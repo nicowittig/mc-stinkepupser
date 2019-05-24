be4e3b33db6b88c8e1b6458349c5cb4e62ce353a
Nico Wittig <nw51raxa@studserv.uni-leipzig.de>
2019-05-24 23:19:43 +0200

    [READY]

--- start_spigot.sh
+++ start_spigot.sh
@@ -2,5 +2,6 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 cd $DIRECTORY
 git pull
+git status
 cd spigot
 java -Xmx1024M -Xms1024M -jar papermc-server.jar
--- start_tekkit-lite.sh
+++ start_tekkit-lite.sh
@@ -2,5 +2,6 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 cd $DIRECTORY
 git pull
+git status
 cd tekkit-lite
-java -Xmx1024M -Xms1024M -jar TekkitLite.jar nogui
+java -Xmx1024M -Xms1024M -jar TekkitLite.jar
